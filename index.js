c8340be9bd2c14a833ef088d4286a7872799a6e5
RazByan <razo.babayan@mail.ru>
2022-03-24 22:20:38 +0400

    solving intern tasks

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const WINNING_SUM = 123.40;
 @param {Array} cartItemsPrices
 */
 function doesCartWinPrize(cartItemsPrices) {
-  
+    let sum = 0;
+
+    for (let i = 0; i < cartItemsPrices.length; i++)
+    {
+        sum += parseFloat(cartItemsPrices[i]);
+    }
+
+    if (parseFloat(sum.toFixed(2)) === WINNING_SUM) return true;
+
+    return false;
 }
 
 module.exports = doesCartWinPrize;
